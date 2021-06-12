6728b6ea9b9e80897f720d041aa54f04fc8d3695
Toby Wiedenhoefer <toby.wiedenh@gmail.com>
2021-06-12 17:14:45 -0500

    Log In and Sign Up links on splash.

--- main.py
+++ main.py
@@ -14,6 +14,16 @@ def splash():
     return render_template('splash.html', title="Gift Gopher")
 
 
+@app.route('/login')
+def login():
+    return "login page."
+
+
+@app.route('/signup')
+def signup():
+    return "Signup page."
+
+
 if __name__ == "__main__":
     app.run(
         debug=True
